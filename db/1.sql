6589fe4894288dc17eb8bd749c54db2fae1d239a
zetta-cai <cjztcl@163.com>
2022-11-29 20:16:36 +0800

    cjz

---
+++ db/1.sql
@@ -0,0 +1,8 @@
+-- create database jsweb;
+create table test(
+   user_id INT NOT NULL AUTO_INCREMENT,
+   user_name VARCHAR(100) NOT NULL,
+   user_password VARCHAR(40) NOT NULL,
+   PRIMARY KEY ( user_id )
+);
+INSERT INTO test (user_id, user_name, user_password) VALUES (14, 'root', '123123');
